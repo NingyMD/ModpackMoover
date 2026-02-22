5d030bce8e24f947a8e981b82f945d99c4faf621
NingyMD <86799484+NingyMD@users.noreply.github.com>
2026-02-22 02:22:38 -0800

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  selectFolder: () => ipcRenderer.invoke('dialog:select-folder'),   // ← changed here
+  selectFolder: () => ipcRenderer.invoke('dialog:select-folder'),
   transferConfigs: (data) => ipcRenderer.invoke('transfer:configs', data)
 });
